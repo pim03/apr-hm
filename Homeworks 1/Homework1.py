171a06db2140dce1dc1fb855d59e3f3f25d50932
Rodrigo Laia <rodrigo.laia@tecnico.ulisboa.pt>
2023-09-28 12:20:27 +0100

    palavras2

--- Homeworks 1/Homework1.py
+++ Homeworks 1/Homework1.py
@@ -4,11 +4,13 @@ from sklearn import metrics, datasets, tree
 from sklearn.model_selection import train_test_split
 import pandas as pd, numpy as np
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, OneHotEncoder
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.feature_selection import f_classif
 import seaborn as sns
+import graphviz
+
 
 # Reading the ARFF file
 data = loadarff('column_diagnosis.arff')
@@ -18,7 +20,7 @@ df['class'] = df['class'].str.decode('utf-8')
 
 ### Exercise 1 ###
 
-''' features = df.drop('class', axis=1)
+features = df.drop('class', axis=1)
 target = df['class']
 
 fvalues, pvalues = f_classif(features, target)
@@ -27,11 +29,11 @@ columns = df.columns
 for i in range(6):
     print('the f-value and the p-value of', df.columns[i], 'is:', fvalues[i], 'and', pvalues[i], 'respectively')
 
-the greater the f-value the better
+'''the greater the f-value the better
 the lower the p-value the better
 
 degree_spondylolisthesis is the variable with higher discriminative power.
-pelvic_radius is the variable with lower discriminative power.
+pelvic_radius is the variable with lower discriminative power.'''
 
 new_features = df.drop('class', axis=1)
 new_features = df.drop('pelvic_incidence', axis=1)
@@ -66,15 +68,15 @@ plt.title('Funções de Densidade de Probabilidade para pelvic_radius')
 plt.show()
 
 #Final Graphic
-sns.pairplot(df, hue='class', height=2)
-plt.show() '''
+#sns.pairplot(df, hue='class', height=2)
+#plt.show()
 
 
 ### Exercise 2 ###
-#variables = df.drop('class', axis= 1)
-#target = df['class']
+variables = df.drop('class', axis= 1)
+target = df['class']
 
-'''depth_limits = [1, 2, 3, 4, 5, 6, 8, 10]
+depth_limits = [1, 2, 3, 4, 5, 6, 8, 10]
 
 variables_train, variables_test, target_train, target_test= train_test_split(variables, target, 
                                                                              train_size=0.7, stratify=target, random_state=0)
@@ -85,11 +87,11 @@ std1, std2 = np.array([]), np.array([])
 for depth in depth_limits:
     acc_folder1, acc_folder2 = np.array([]), np.array([])
     for i in range(len(depth_limits)):
-        tree = DecisionTreeClassifier(criterion='gini', max_depth=depth)
-        tree.fit(variables_train, target_train)
+        tree2 = DecisionTreeClassifier(criterion='gini', max_depth=depth)
+        tree2.fit(variables_train, target_train)
         
-        y_pred1 = tree.predict(variables_test)
-        y_pred2 = tree.predict(variables_train)
+        y_pred1 = tree2.predict(variables_test)
+        y_pred2 = tree2.predict(variables_train)
         
         acc_folder2 = np.append(acc_folder2, round(metrics.accuracy_score(target_train, y_pred2),2))
         acc_folder1 = np.append(acc_folder1, round(metrics.accuracy_score(target_test, y_pred1),2))
@@ -102,18 +104,19 @@ for depth in depth_limits:
 print("accuracy test list:", final_acc1)
 print("accuracy train list:", final_acc2)
 
-#print('std1',std1)
+
 #Graphics
 plt.figure(figsize=(12,7))
-#tenho de por pontos ou não?
 plt.errorbar(depth_limits, final_acc1, yerr=std1, label='Test Accuracy', color='#008080')
 plt.errorbar(depth_limits, final_acc2, yerr=std2, label='Train Accuracy', color='red')
+plt.plot(depth_limits, final_acc1, 'o', color='#008080', markersize=3)
+plt.plot(depth_limits, final_acc2, 'o', color='red', markersize=3)
 plt.title("Accuracy vs Depth")
 plt.ylabel("Training Accuracy")
 plt.xlabel("Depth")
 plt.ylim(0.73, 1.03)
 plt.legend() #por legenda a esquerda
-plt.show() '''
+plt.show() 
 
 ### Exercise 3 ###
 x_train = df.drop('class', axis= 1)
@@ -127,7 +130,6 @@ print('accuracy:', round(metrics.accuracy_score(y_train, target_pred), 2))
 
 class_names = ['Hernia', 'Spondylolisthesis', 'Normal']
 
-plot = tree.plot_tree(tree4, filled=True, feature_names=x_train.columns, class_names=tree4.classes_)
+tree.plot_tree(tree4, filled=True, feature_names=x_train.columns, class_names=tree4.classes_)
 plt.show()
-
 
