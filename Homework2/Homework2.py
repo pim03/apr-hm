1bd1635774e0b5533611d50546d648af5a8d8f91
pim03 <132604887+pim03@users.noreply.github.com>
2023-10-06 17:27:35 +0100

    homework2.py corrigido

--- Homework2/Homework2.py
+++ Homework2/Homework2.py
@@ -128,39 +128,66 @@ else:
 
 ############ Exercise 2 ############
 
-# Load your dataset and split it into training and testing sets
-# X_train, X_test, y_train, y_test = ...
+print('--------------Exercise 2-----------------')
 
-# Train kNN classifiers with k=1 and k=5
-knn1 = KNeighborsClassifier(n_neighbors=1,uniform=True,metric='euclidean')
-knn5 = KNeighborsClassifier(n_neighbors=5,uniform=True,metric='euclidean')
+#Initialize the cumulative confusion matrices
+cum_conf_matrix1 = np.zeros((3,3))
+cum_conf_matrix5 = np.zeros((3,3))
 
-knn1.fit(X_train, y_train)
-knn5.fit(X_train, y_train)
+for train_k, test_k in folds.split(features, target):
+    X_train, X_test = features.iloc[train_k], features.iloc[test_k]
+    y_train, y_test = target.iloc[train_k], target.iloc[test_k]
+    
+    ## train and assess
+    knn1 = KNeighborsClassifier(n_neighbors=1,weights='uniform',metric='euclidean')
+    knn5 = KNeighborsClassifier(n_neighbors=5,weights='uniform',metric='euclidean')
 
-# Make predictions
-y_pred1 = knn1.predict(X_test)
-y_pred5 = knn5.predict(X_test)
+    knn1.fit(X_train, y_train)
+    knn5.fit(X_train, y_train)
 
-# Calculate confusion matrices
-conf_matrix1 = confusion_matrix(y_test, y_pred1)
-conf_matrix5 = confusion_matrix(y_test, y_pred5)
+    # Make predictions
+    y_pred1 = knn1.predict(X_test)
+    y_pred5 = knn5.predict(X_test)
+    
+    knn_predictor.fit(X_train, y_train)
+    y_pred_knn = knn_predictor.predict(X_test)
+    acc_folds_knn.append(round(metrics.accuracy_score(y_test, y_pred_knn),2))
 
-# Calculate cumulative confusion matrices
-cumulative_conf_matrix1 = np.cumsum(conf_matrix1, axis=0)
-cumulative_conf_matrix5 = np.cumsum(conf_matrix5, axis=0)
+    # Calculate confusion matrices
+    conf_matrix1 = confusion_matrix(y_test, y_pred1)
+    conf_matrix5 = confusion_matrix(y_test, y_pred5)
 
-# Calculate the difference between cumulative confusion matrices
-conf_matrix_diff = cumulative_conf_matrix1 - cumulative_conf_matrix5
+    # Calculate cumulative confusion matrices
+    cum_conf_matrix1 += conf_matrix1
+    cum_conf_matrix5 += conf_matrix5
 
-# Visualize the differences using a heatmap
-plt.figure(figsize=(8, 6))
-sns.heatmap(conf_matrix_diff, annot=True, fmt="d", cmap="coolwarm", cbar=True)
-plt.xlabel("Predicted")
-plt.ylabel("Actual")
-plt.title("Difference in Cumulative Confusion Matrices (k=1 - k=5)")
+#Calculate the difference between the two confusion matrices
+conf_matrix_diff = cum_conf_matrix1 - cum_conf_matrix5
+
+#cm1 = np.array(confusion_matrix(y_test, y_pred1))
+confusion1 = pd.DataFrame(conf_matrix_diff, index=knn1.classes_, columns=['Predicted Hernia', 'Predicted Normal', 'Predicted Spondylolisthesis'])
+print(confusion1)
+# ... (previous code for conf_matrix_diff and heatmap setup)
+
+# Create the heatmap with a color bar
+plt.figure(figsize=(10, 5))
+heatmap = plt.imshow(conf_matrix_diff,cmap="coolwarm", interpolation='nearest')
+plt.title('Differences between the two cumulative confusion matrices (k1 - k5)')
+plt.xlabel('Predicted label')
+plt.xticks([0, 1, 2], ['Hernia', 'Normal', 'Spondylolisthesis'])
+plt.yticks([0, 1, 2], ['Hernia', 'Normal', 'Spondylolisthesis'])
+plt.ylabel('True label')
+
+# Add a color bar legend
+cbar = plt.colorbar(heatmap)
+cbar.set_label('Difference Magnitude', rotation=90)
+
+# Display numerical values within each cell
+for i in range(conf_matrix_diff.shape[0]):
+    for j in range(conf_matrix_diff.shape[1]):
+        plt.text(j, i, str(int(conf_matrix_diff[i, j])), ha='center', va='center', color='black')
+
 plt.show()
-
 
 
     
